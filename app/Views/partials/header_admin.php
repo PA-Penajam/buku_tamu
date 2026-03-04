9a22cc4b48780f5747d8f5155b31ffbfbb81d248
Moohard <dian.kuba13@gmail.com>
2026-03-05 06:35:35 +0800

    fix(ui): correct metronic 8 menu html structure to show navigation links

--- app/Views/partials/header_admin.php
+++ app/Views/partials/header_admin.php
@@ -8,28 +8,28 @@
         </div>
         <div class="d-flex align-items-stretch justify-content-between flex-lg-grow-1">
             <div class="d-flex align-items-stretch" id="kt_header_nav">
-                <div class="header-menu align-items-stretch" data-kt-drawer="true" data-kt-drawer-name="header-menu" data-kt-drawer-activate="{default: true, lg: false}" data-kt-drawer-overlay="true" data-kt-drawer-width="{default:'200px', '300px': '250px'}" data-kt-drawer-direction="end" data-kt-drawer-toggle="#kt_header_menu_mobile_toggle" data-kt-swapper="true" data-kt-swapper-mode="prepend" data-kt-swapper-parent="{default: '#kt_body', lg: '#kt_header_nav'}">
+                <div class="header-menu align-items-stretch">
                     <div class="menu menu-lg-rounded menu-column menu-lg-row menu-state-bg menu-title-gray-700 menu-state-title-primary menu-state-icon-primary menu-state-bullet-primary menu-arrow-gray-400 fw-bold my-5 my-lg-0 align-items-stretch" id="#kt_header_menu" data-kt-menu="true">
-                        <a href="/admin" class="menu-item <?= current_url() == base_url('admin') ? 'here show' : '' ?> menu-lg-down-accordion me-lg-1">
-                            <span class="menu-link py-3">
+                        <div class="menu-item <?= current_url() == base_url('admin') ? 'here show' : '' ?> menu-lg-down-accordion me-lg-1">
+                            <a href="/admin" class="menu-link py-3">
                                 <span class="menu-title">Dashboard</span>
-                            </span>
-                        </a>
-                        <a href="/admin/tamu" class="menu-item <?= strpos(current_url(), '/admin/tamu') !== false ? 'here show' : '' ?> menu-lg-down-accordion me-lg-1">
-                            <span class="menu-link py-3">
+                            </a>
+                        </div>
+                        <div class="menu-item <?= strpos(current_url(), '/admin/tamu') !== false ? 'here show' : '' ?> menu-lg-down-accordion me-lg-1">
+                            <a href="/admin/tamu" class="menu-link py-3">
                                 <span class="menu-title">Tamu</span>
-                            </span>
-                        </a>
-                        <a href="/admin/pengunjung" class="menu-item <?= strpos(current_url(), '/admin/pengunjung') !== false ? 'here show' : '' ?> menu-lg-down-accordion me-lg-1">
-                            <span class="menu-link py-3">
+                            </a>
+                        </div>
+                        <div class="menu-item <?= strpos(current_url(), '/admin/pengunjung') !== false ? 'here show' : '' ?> menu-lg-down-accordion me-lg-1">
+                            <a href="/admin/pengunjung" class="menu-link py-3">
                                 <span class="menu-title">Pengunjung</span>
-                            </span>
-                        </a>
-                        <a href="/admin/laporan" class="menu-item <?= strpos(current_url(), '/admin/laporan') !== false ? 'here show' : '' ?> menu-lg-down-accordion me-lg-1">
-                            <span class="menu-link py-3">
+                            </a>
+                        </div>
+                        <div class="menu-item <?= strpos(current_url(), '/admin/laporan') !== false ? 'here show' : '' ?> menu-lg-down-accordion me-lg-1">
+                            <a href="/admin/laporan" class="menu-link py-3">
                                 <span class="menu-title">Laporan</span>
-                            </span>
-                        </a>
+                            </a>
+                        </div>
                     </div>
                 </div>
             </div>
